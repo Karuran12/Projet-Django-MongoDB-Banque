86bcc290e7c8e2012f1c03d5f982f05763f92faa
dynanoir <tom.guittet0@gmail.com>
2025-11-27 12:53:03 +0100

    register enlevé

--- bank_project/core/urls.py
+++ bank_project/core/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
+    path('', views.home_view, name='login'),
     path('login/', views.login_view, name='login'),
-    path('', views.login_view),  
-
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- bank_project/core/views.py
+++ bank_project/core/views.py
@@ -1,19 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, authenticate, logout
-
-def register_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('/dashboard')  # Redirection après inscription
-    else:
-        form = UserCreationForm()
-    
-    return render(request, 'auth/register.html', {'form': form})
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 
 def login_view(request):
     if request.method == 'POST':
@@ -21,9 +9,37 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('/dashboard')
+            return redirect('dashboard')
     else:
         form = AuthenticationForm()
-    
+
     return render(request, 'auth/login.html', {'form': form})
 
+
+@login_required
+def home_view(request):
+    username = request.user.username
+    is_admin = request.user.is_staff
+
+    return render(request, "home.html", {
+        "username": username,
+        "is_admin": is_admin,
+        "user_role": "Administrateur" if is_admin else "Utilisateur"
+    })
+
+
+@login_required
+def dashboard_view(request):
+    username = request.user.username
+    is_admin = request.user.is_staff
+
+    return render(request, "dashboard.html", {
+        "username": username,
+        "is_admin": is_admin,
+        "user_role": "Administrateur" if is_admin else "Utilisateur"
+    })
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
